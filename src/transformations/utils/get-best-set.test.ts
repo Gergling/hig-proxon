979f854de4ef10f6442e769094941d21387db569
Greg <gregmichaeldavies@gmail.com>
2025-06-28 20:17:18 +0100

    feat: Added function for returning the best set when comparing two.

---
+++ src/transformations/utils/get-best-set.test.ts
@@ -0,0 +1,46 @@
+import { mockGymExerciseSet } from "../../gym-trips/mock-data";
+import { GymExerciseSet } from "../../types";
+import { getBestSet } from "./get-best-set";
+
+const mockSetZero: GymExerciseSet = {
+  ...mockGymExerciseSet,
+  reps: 0,
+  validity: 'zero',
+};
+const mockSetInvalid: GymExerciseSet = {
+  ...mockGymExerciseSet,
+  reps: 6,
+  validity: 'invalid',
+};
+const mockSetLowReps: GymExerciseSet = {
+  ...mockGymExerciseSet,
+  reps: 7,
+  validity: 'valid',
+};
+const mockSetHighReps: GymExerciseSet = {
+  ...mockSetLowReps,
+  reps: 15,
+};
+const mockSetHighChallenge: GymExerciseSet = {
+  ...mockSetLowReps,
+  challenge: 3,
+};
+
+describe('getBestSet', () => {
+  it('should return the set which is more valid, if they have varying levels of validity', () => {
+    expect(getBestSet(mockSetZero, mockSetInvalid)).toBe(mockSetInvalid);
+    expect(getBestSet(mockSetInvalid, mockSetZero)).toBe(mockSetInvalid);
+    expect(getBestSet(mockSetZero, mockSetLowReps)).toBe(mockSetLowReps);
+    expect(getBestSet(mockSetLowReps, mockSetZero)).toBe(mockSetLowReps);
+    expect(getBestSet(mockSetInvalid, mockSetLowReps)).toBe(mockSetLowReps);
+    expect(getBestSet(mockSetLowReps, mockSetInvalid)).toBe(mockSetLowReps);
+  });
+  it('should return the set which has reached a higher challenge, if they are both valid sets', () => {
+    expect(getBestSet(mockSetHighReps, mockSetHighChallenge)).toBe(mockSetHighChallenge);
+    expect(getBestSet(mockSetHighChallenge, mockSetHighReps)).toBe(mockSetHighChallenge);
+  });
+  it('should return the set which has reached a higher number of reps, if they are both valid and have the same challenge', () => {
+    expect(getBestSet(mockSetLowReps, mockSetHighReps)).toBe(mockSetHighReps);
+    expect(getBestSet(mockSetHighReps, mockSetLowReps)).toBe(mockSetHighReps);
+  });
+});
---
+++ src/transformations/utils/get-best-set.ts
@@ -0,0 +1,24 @@
+import { GymExerciseSet } from "../../types";
+import { getSetValidityComparison } from "./get-set-validity-comparison";
+
+export const getBestSet = (
+  a: GymExerciseSet,
+  b: GymExerciseSet
+): GymExerciseSet => {
+  // We *might* be comparing sets which aren't valid. If they are not equally
+  // valid, we should return the one which is "more" valid, e.g. because it
+  // has a rep rather than *no* reps.
+  const validityComparison = getSetValidityComparison(a.validity, b.validity);
+
+  if (validityComparison !== 0)
+    return validityComparison < 0 ? b : a;
+
+  // If both sets are of equal validity, regardless of what that level is,
+  if (a.challenge > b.challenge) return a;
+  if (a.challenge < b.challenge) return b;
+
+  if (a.reps > b.reps) return a;
+  if (a.reps < b.reps) return b;
+
+  return a;
+};
