febfd98a5841461d41914742b4be8bd19a0514d0
Greg <gregmichaeldavies@gmail.com>
2025-07-06 13:39:54 +0100

    tech: Utilised separated functions for ETL.

--- src/backfill/script.ts
+++ src/backfill/script.ts
@@ -1,7 +1,16 @@
-import { extractAll } from "../extract/extract-all";
+import { transformAll } from "../transformations/transform-all";
+import { extract } from "./extract";
+import { load } from "./load";
 
 export const backfill = async () => {
-  const dtos = await extractAll();
-  // transform
-  // load
+  try {
+    const dtos = await extract();
+    const data = transformAll(dtos);
+    await load(data);
+  } catch(e) {
+    console.error('! Backfill failed:', e);
+    throw e;
+  }
 };
+
+backfill();
