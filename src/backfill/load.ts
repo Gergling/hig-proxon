83eab0d7789cf061b524f6c5d72679573ecb2d91
Greg <gregmichaeldavies@gmail.com>
2025-07-06 13:39:35 +0100

    tech: Separated load function for ETL.

---
+++ src/backfill/load.ts
@@ -0,0 +1,21 @@
+import { store } from "../load";
+import { GymTripProps, MuscleGroup } from "../types";
+
+export const load = async ({
+  muscleGroups,
+  trips,
+}: {
+  muscleGroups: MuscleGroup[];
+  trips: GymTripProps[];
+}) => {
+  console.log('+++ Loading...');
+  try {
+    await store({
+      muscleGroups,
+      trips,
+    });
+  } catch (e) {
+    console.error('! Loading failed:', e);
+    throw e;
+  }
+};
