d5c37fed39d6198ec6053ed72b2eec120361bdec
Greg <gregmichaeldavies@gmail.com>
2025-07-26 15:06:14 +0100

    tech: Included error catch for equipment DTO.

--- src/data/transformations/dto/get-equipment.ts
+++ src/data/transformations/dto/get-equipment.ts
@@ -1,7 +1,16 @@
 import { ExerciseEquipmentResponseDTO } from "../../../notion-sdk/dbs/exercise-equipment";
 import { getNameProperty } from "./get-name-property";
 
-export const getEquipment = ({
-  id,
-  properties: { name }
-}: ExerciseEquipmentResponseDTO) => ({ name: getNameProperty(name), id });
+export const getEquipment = (dto: ExerciseEquipmentResponseDTO) => {
+  const {
+    id,
+    properties: { name }
+  } = dto;
+  if (!name) {
+    console.error(`Equipment record with id '${id}' has no name property. Full DTO data is as follows:`, dto);
+  };
+  return {
+    name: getNameProperty(name),
+    id
+  }
+};
