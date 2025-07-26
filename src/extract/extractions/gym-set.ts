65106646d7d61c8ec03d66fa5eb5e6bd512eedab
Greg <gregmichaeldavies@gmail.com>
2025-07-26 15:57:30 +0100

    tech: Removed superfluous comments.

--- src/extract/extractions/gym-set.ts
+++ src/extract/extractions/gym-set.ts
@@ -1,20 +1,5 @@
 import { GymSetDatabase, GymSetQueryResponse, GymSetResponseDTO } from "../../notion-sdk/dbs/gym-set";
 import { createDbInstance, query } from "../core";
-// import { DatabaseConstructor } from "../types";
-
-// const queryFnFactory = <DbQueryResponse>(
-//   fn: (start_cursor: string | undefined) => Promise<DbQueryResponse>
-// ) => async (
-//   start_cursor: string | undefined
-// ) => {
-//   const response = await db.query({ start_cursor });
-//   const { has_more, next_cursor } = response;
-//   return {
-//     has_more,
-//     next_cursor,
-//     response,
-//   };
-// }
 
 export const extractGymSet = async (notionSecret: string) => {
   const db = createDbInstance(GymSetDatabase, notionSecret);
