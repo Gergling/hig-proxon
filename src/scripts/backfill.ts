5f597d7e9b3a885a714c27087ff2c10df0d98704
Greg <gregmichaeldavies@gmail.com>
2025-08-03 12:23:10 +0100

    fix: Using correct extraction function.

--- src/scripts/backfill.ts
+++ src/scripts/backfill.ts
@@ -1,10 +1,11 @@
-import { extractAll } from "../data";
+import { extractAll } from "../data/extraction/notion/extract-all";
 import { store } from "../data/load";
 import { transformAll } from "../data/transformations/transform-all-gym-visits";
+import { DataDtoProps } from "../data/types/notion";
 
 export const backfill = async () => {
   try {
-    const dtos = await extractAll();
+    const dtos = await extractAll() as DataDtoProps;
     const data = transformAll(dtos);
     await store(data);
   } catch(e) {
